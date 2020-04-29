4636a23a15cfb8cb6214e7f54917e313ba5dc548
Evan You <yyx990803@gmail.com>
2020-04-28 21:15:30 -0400

    debug layout

--- lib/theme-default/Layout.vue
+++ lib/theme-default/Layout.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="theme-container">
-    <h1>Hello VitePress</h1>
+    <h1>{{ $site.title }}</h1>
+    <p>{{ $site.description }}</p>
+    <Content/>
+  </div>
+  <div class="debug">
     <pre>$site {{ $site }}</pre>
     <pre>$page {{ $page }}</pre>
     <pre>useSiteData() {{ site }}</pre>
     <pre>usePageData() {{ page }}</pre>
-    <Content/>
   </div>
 </template>
 
@@ -25,5 +28,15 @@ export default {
 <style>
 .theme-container {
   font-family: Arial, Helvetica, sans-serif;
+}
+
+.debug {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 400px;
+  color: #fff;
+  background-color: rgba(0,0,0,0.85);
+  padding: 10px 20px;
 }
 </style>
