d8f2eb1ffc8e66c2481667397cc9907feae516b4
Evan You <yyx990803@gmail.com>
2020-05-22 17:44:44 -0400

    tweap copy script

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,30 +1,13 @@
 // copy and watch non-ts files in src/client
 const fs = require('fs-extra')
-const path = require('path')
-const globby = require('globby')
 const chokidar = require('chokidar')
-
-const pattern = 'src/client/**/!(*.ts|tsconfig.json)'
 
 function toDest(file) {
   return file.replace(/^src\//, 'dist/')
 }
 
-function copy(file) {
-  fs.copy(file, toDest(file))
-}
-
-// copy on start
-;(async () => {
-  for (const file of await globby(pattern)) {
-    copy(file)
-  }
-})()
-
 chokidar
-  .watch(pattern)
-  .on('change', copy)
-  .on('add', copy)
-  .on('unlink', (file) => {
-    fs.remove(toDest(file))
-  })
+  .watch('src/client/**/!(*.ts|tsconfig.json)')
+  .on('change', (file) => fs.copy(file, toDest(file)))
+  .on('add', (file) => fs.copy(file, toDest(file)))
+  .on('unlink', (file) => fs.remove(toDest(file)))
