eeef64e11c2a1242d374ec4080434afdfe193d42
Evan You <yyx990803@gmail.com>
2020-05-22 17:48:46 -0400

    fix theme typing

--- src/client/theme-default/index.ts
+++ src/client/theme-default/index.ts
@@ -1,19 +1,5 @@
-import { App, Ref, ComponentOptions } from 'vue'
 import Layout from './Layout.vue'
-import { Router } from '/@app/router'
-import { SiteData } from '../../../types/shared'
-
-export interface EnhanceAppContext {
-  app: App
-  router: Router
-  siteData: Ref<SiteData>
-}
-
-export interface Theme {
-  Layout: ComponentOptions
-  NotFound?: ComponentOptions
-  enhanceApp?: (ctx: EnhanceAppContext) => void
-}
+import { Theme } from '../app/theme'
 
 const theme: Theme = {
   Layout
