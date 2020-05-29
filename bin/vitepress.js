9b6aebbaacb10e9a21e6671bc9bef1b331a915a5
zuofenghua <11100776@bbktel.com>
2020-05-29 11:55:18 +0800

    fix: silent failure when port is used

--- bin/vitepress.js
+++ bin/vitepress.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 const argv = require('minimist')(process.argv.slice(2))
+const portfinder = require('portfinder')
 
 console.log(chalk.cyan(`vitepress v${require('../package.json').version}`))
 console.log(chalk.cyan(`vite v${require('vite/package.json').version}`))
@@ -8,14 +9,15 @@ console.log(chalk.cyan(`vite v${require('vite/package.json').version}`))
 const command = argv._[0]
 
 if (!command || command === 'dev') {
-  const port = argv.port || 3000
   const root = command === 'dev' && argv._[1]
   if (root) {
     argv.root = root
   }
   require('../dist/node')
     .createServer(argv)
-    .then((server) => {
+    .then(async (server) => {
+      portfinder.basePort = parseInt(argv.port) || 3000
+      const port = await portfinder.getPortPromise()
       server.listen(port, () => {
         console.log(`listening at http://localhost:${port}`)
       })
