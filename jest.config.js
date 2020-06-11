96b2424e4dc76413c53a5655185a83041cdfb89e
An Phan <me@phanan.net>
2020-06-11 22:03:39 +0200

    rebase with upstream

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
-      tsConfig: 'src/tsconfig.json'
+      tsConfig: './tsconfig.base.json'
     }
   },
   moduleNameMapper: {
