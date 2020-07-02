36b8696562bc7f3eccb3d12fcf3070bab75ee8fe
Kia Ishii <kia.king.08@gmail.com>
2020-07-02 23:29:17 +0900

    feat: add array sidebar support

--- src/client/theme-default/utils.ts
+++ src/client/theme-default/utils.ts
@@ -1,5 +1,23 @@
-import { useSiteData } from 'vitepress'
+import { useSiteData, Route } from 'vitepress'
+
+export const hashRE = /#.*$/
+export const extRE = /\.(md|html)$/
 
 export function withBase(path: string) {
   return (useSiteData().value.base + path).replace(/\/+/g, '/')
+}
+
+export function isActive(route: Route, path?: string): boolean {
+  if (path === undefined) {
+    return false
+  }
+
+  const routePath = normalize(route.path)
+  const pagePath = normalize(path)
+
+  return routePath === pagePath
+}
+
+export function normalize(path: string): string {
+  return decodeURI(path).replace(hashRE, '').replace(extRE, '')
 }
