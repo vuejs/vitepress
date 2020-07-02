6f10ed6c63b7486f678fdd7eedc888925feb473c
Eduardo San Martin Morote <posva@users.noreply.github.com>
2020-07-02 22:10:11 +0200

    fix: use resolve instead of join (#33)
    Allows the documentation to exist in a folder e.g. `docs`, and running `vitepress dev
    docs` as well as `vitepress build docs`

--- bin/vitepress.js
+++ bin/vitepress.js
@@ -6,13 +6,13 @@ console.log(chalk.cyan(`vitepress v${require('../package.json').version}`))
 console.log(chalk.cyan(`vite v${require('vite/package.json').version}`))
 
 const command = argv._[0]
+const root = argv._[command ? 1 : 0]
+if (root) {
+  argv.root = root
+}
 
 if (!command || command === 'dev') {
   const port = argv.port || 3000
-  const root = command === 'dev' && argv._[1]
-  if (root) {
-    argv.root = root
-  }
   require('../dist/node')
     .createServer(argv)
     .then((server) => {
