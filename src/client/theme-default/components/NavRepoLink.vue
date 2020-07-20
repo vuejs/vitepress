c3be5472e64ed82b9688573c65490cea74830e54
Shintaro Tanaka <s-tanaka@holmescloud.com>
2020-07-21 01:29:51 +0900

    feat: add git repo link

---
+++ src/client/theme-default/components/NavRepoLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a
+    v-if="repoLink"
+    :href="repoLink"
+    class="repo-link"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {{ repoLabel }}
+    <OutboundLink />
+  </a>
+</template>
+
+<script src="./NavRepoLink"></script>
+
+<style>
+.repo-link {
+  margin-left: 1.5rem;
+  color: var(--text-color);
+  font-weight: 600;
+}
+</style>
