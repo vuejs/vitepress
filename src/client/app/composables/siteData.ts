583f02e488845e106a2f43232e19a398b902b651
Evan You <yyx990803@gmail.com>
2020-07-21 15:45:50 -0400

    chore: bump deps

--- src/client/app/composables/siteData.ts
+++ src/client/app/composables/siteData.ts
@@ -2,7 +2,7 @@ import serialized from '@siteData'
 import { ref, readonly, Ref } from 'vue'
 import { SiteData } from '../../../../types/shared'
 
-const parse = (data: string) => readonly(JSON.parse(data) as SiteData)
+const parse = (data: string) => readonly(JSON.parse(data)) as SiteData
 
 export const siteDataRef: Ref<SiteData> = ref(parse(serialized))
 
