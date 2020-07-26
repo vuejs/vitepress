1be254dbc7a7f3f56038a3de2376017bfc546f3f
Yugo Ogura <jigenbakdaaan@gmail.com>
2020-07-26 22:24:24 +0900

    feat: disable links when explicity set false

--- src/client/theme-default/components/NextAndPrevLinks.ts
+++ src/client/theme-default/components/NextAndPrevLinks.ts
@@ -5,9 +5,15 @@ export default defineComponent({
   setup() {
     const pageData = usePageData()
     const next = computed(() => {
+      if (pageData.value.frontmatter.next === false) {
+        return undefined
+      }
       return pageData.value.next
     })
     const prev = computed(() => {
+      if (pageData.value.frontmatter.prev === false) {
+        return undefined
+      }
       return pageData.value.prev
     })
     const hasLinks = computed(() => {
