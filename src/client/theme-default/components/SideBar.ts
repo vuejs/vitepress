15eb82152638f9af086cd6caf2169efa18946a5a
pikax <carlos@hypermob.co.uk>
2020-08-27 13:26:00 +0100

    fix(sidebar): fix sidebar when you open a nested link

--- src/client/theme-default/components/SideBar.ts
+++ src/client/theme-default/components/SideBar.ts
@@ -129,7 +129,8 @@ function resolveMultiSidebar(
   headers: Header[],
   depth: number
 ): ResolvedSidebar {
-  const item = config[getPathDirName(path)]
+  const paths = [path, Object.keys(config)[0]]
+  const item = paths.map((x) => config[getPathDirName(x)]).find(Boolean)
 
   if (Array.isArray(item)) {
     return resolveArraySidebar(item, depth)
