fb09f8e638c06aec32494f731554fb1b989daaf0
Evan You <yyx990803@gmail.com>
2020-11-19 13:05:54 -0500

    fix: exit process with non-zero code on error

--- bin/vitepress.js
+++ bin/vitepress.js
@@ -22,13 +22,16 @@ if (!command || command === 'dev') {
     })
     .catch((err) => {
       console.error(chalk.red(`failed to start server. error:\n`), err)
+      process.exit(1)
     })
 } else if (command === 'build') {
   require('../dist/node')
     .build(argv)
     .catch((err) => {
       console.error(chalk.red(`build error:\n`), err)
+      process.exit(1)
     })
 } else {
   console.log(chalk.red(`unknown command "${command}".`))
+  process.exit(1)
 }
