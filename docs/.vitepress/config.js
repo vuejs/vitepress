08480c7fba0bc0b2395cab755bc4a87389492d1a
Kia King Ishii <kia.king.08@gmail.com>
2020-11-21 02:32:27 +0900

    docs: make sidebar config a bit more organized

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,19 +20,28 @@ module.exports = {
     ],
 
     sidebar: {
-      '/guide/': [
-        {
-          text: 'Introduction',
-          children: [
-            { text: 'What is VitePress?', link: '/' },
-            { text: 'Getting Started', link: '/guide/getting-started' },
-            { text: 'Configuration', link: '/guide/configuration' },
-            { text: 'Customization', link: '/guide/customization' },
-            { text: 'Deploying', link: '/guide/deploy' }
-          ]
-        }
-      ],
-      '/config/': [{ text: 'Config Reference', link: '/config/' }]
+      '/': getGuideSidebar(),
+      '/guide/': getGuideSidebar(),
+      '/config/': getConfigSidebar()
     }
   }
+}
+
+function getGuideSidebar() {
+  return [
+    {
+      text: 'Introduction',
+      children: [
+        { text: 'What is VitePress?', link: '/' },
+        { text: 'Getting Started', link: '/guide/getting-started' },
+        { text: 'Configuration', link: '/guide/configuration' },
+        { text: 'Customization', link: '/guide/customization' },
+        { text: 'Deploying', link: '/guide/deploy' }
+      ]
+    }
+  ]
+}
+
+function getConfigSidebar() {
+  return [{ text: 'Config Reference', link: '/config/' }]
 }
