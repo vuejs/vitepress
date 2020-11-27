77bb75f7eec8f149e32a52d66be76c21d1f282c9
Kia King Ishii <kia.king.08@gmail.com>
2020-11-27 17:52:46 +0900

    refactor: client app index

--- src/client/app/index.ts
+++ src/client/app/index.ts
@@ -1,6 +1,6 @@
-import { createApp as createClientApp, createSSRApp } from 'vue'
+import { App, createApp as createClientApp, createSSRApp } from 'vue'
 import { inBrowser, pathToFile } from './utils'
-import { createRouter, RouterSymbol } from './router'
+import { Router, RouterSymbol, createRouter } from './router'
 import { mixinGlobalComputed, mixinGlobalComponents } from './mixin'
 import { siteDataRef } from './composables/siteData'
 import { useSiteDataByRoute } from './composables/siteDataByRoute'
@@ -11,54 +11,13 @@ import Theme from '/@theme/index'
 const NotFound = Theme.NotFound || (() => '404 Not Found')
 
 export function createApp() {
-  let isInitialPageLoad = inBrowser
-  let initialPath: string
+  const router = newRouter()
 
-  const router = createRouter((path) => {
-    let pageFilePath = pathToFile(path)
+  handleHMR(router)
 
-    if (isInitialPageLoad) {
-      initialPath = pageFilePath
-    }
-
-    // use lean build if this is the initial page load or navigating back
-    // to the initial loaded path (the static vnodes already adopted the
-    // static content on that load so no need to re-fetch the page).
-    if (isInitialPageLoad || initialPath === pageFilePath) {
-      pageFilePath = pageFilePath.replace(/\.js$/, '.lean.js')
-    }
-
-    if (inBrowser) {
-      isInitialPageLoad = false
-      // in browser: native dynamic import
-      return import(/*@vite-ignore*/ pageFilePath)
-    } else {
-      // SSR, sync require
-      return require(pageFilePath)
-    }
-  }, NotFound)
-
-  // update route.data on HMR updates of active page
-  if (import.meta.hot) {
-    // hot reload pageData
-    import.meta.hot!.on('vitepress:pageData', (payload) => {
-      if (
-        payload.path.replace(/(\bindex)?\.md$/, '') ===
-        location.pathname.replace(/(\bindex)?\.html$/, '')
-      ) {
-        router.route.data = payload.pageData
-      }
-    })
-  }
-
-  const app =
-    process.env.NODE_ENV === 'production'
-      ? createSSRApp(Theme.Layout)
-      : createClientApp(Theme.Layout)
+  const app = newApp()
 
   app.provide(RouterSymbol, router)
-
-  mixinGlobalComponents(app)
 
   const siteDataByRouteRef = useSiteDataByRoute(router.route)
   const pageDataRef = usePageData(router.route)
@@ -69,6 +28,7 @@ export function createApp() {
   }
 
   mixinGlobalComputed(app, siteDataRef, siteDataByRouteRef, pageDataRef)
+  mixinGlobalComponents(app)
 
   if (Theme.enhanceApp) {
     Theme.enhanceApp({
@@ -79,6 +39,61 @@ export function createApp() {
   }
 
   return { app, router }
+}
+
+function newApp(): App {
+  return process.env.NODE_ENV === 'production'
+    ? createSSRApp(Theme.Layout)
+    : createClientApp(Theme.Layout)
+}
+
+function newRouter(): Router {
+  let isInitialPageLoad = inBrowser
+  let initialPath: string
+
+  return createRouter((path) => {
+    let pageFilePath = pathToFile(path)
+
+    if (isInitialPageLoad) {
+      initialPath = pageFilePath
+    }
+
+    // use lean build if this is the initial page load or navigating back
+    // to the initial loaded path (the static vnodes already adopted the
+    // static content on that load so no need to re-fetch the page)
+    if (isInitialPageLoad || initialPath === pageFilePath) {
+      pageFilePath = pageFilePath.replace(/\.js$/, '.lean.js')
+    }
+
+    // in browser: native dynamic import
+    if (inBrowser) {
+      isInitialPageLoad = false
+
+      return import(/*@vite-ignore*/ pageFilePath)
+    }
+
+    // SSR: sync require
+    return require(pageFilePath)
+  }, NotFound)
+}
+
+function handleHMR(router: Router): void {
+  // update route.data on HMR updates of active page
+  if (import.meta.hot) {
+    // hot reload pageData
+    import.meta.hot!.on('vitepress:pageData', (payload) => {
+      if (shouldHotReload(payload)) {
+        router.route.data = payload.pageData
+      }
+    })
+  }
+}
+
+function shouldHotReload(payload: any): boolean {
+  const payloadPath = payload.path.replace(/(\bindex)?\.md$/, '')
+  const locationPath = location.pathname.replace(/(\bindex)?\.html$/, '')
+
+  return payloadPath === locationPath
 }
 
 if (inBrowser) {
