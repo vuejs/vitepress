2d77eafe3b05e9fe76031af9a6c5386c4c6586ac
Evan You <yyx990803@gmail.com>
2020-12-30 14:40:19 -0500

    feat: production ready serve

--- src/node/serve/serve.ts
+++ src/node/serve/serve.ts
@@ -1,5 +1,5 @@
-import http from 'http'
 import sirv from 'sirv'
+import compression from 'compression'
 import { resolveConfig } from '../config'
 
 export interface ServeOptions {
@@ -11,8 +11,25 @@ export async function serve(options: ServeOptions = {}) {
   const port = options.port !== undefined ? options.port : 5000
   const site = await resolveConfig(options.root)
 
-  const server = http.createServer(sirv(site.outDir, { dev: true, etag: true }))
-  server.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
+  const compress = compression()
+  const serve = sirv(site.outDir, {
+    etag: true,
+    single: true,
+    maxAge: 31536000,
+    immutable: true,
+    setHeaders(res, pathname) {
+      if (!pathname.includes('/assets/')) {
+        // force server validation for non-asset files since they are not
+        // fingerprinted.
+        res.setHeader('cache-control', 'no-cache')
+      }
+    }
   })
+
+  require('polka')()
+    .use(compress, serve)
+    .listen(port, (err: any) => {
+      if (err) throw err
+      console.log(`Built site served at http://localhost:${port}.\n`)
+    })
 }
