60d86e13ef0e8f5f8bce782427c5f6795f6374d9
Evan You <yyx990803@gmail.com>
2020-12-30 12:08:23 -0500

    chore: use different port for serve

--- src/node/serve/serve.ts
+++ src/node/serve/serve.ts
@@ -8,7 +8,7 @@ export interface ServeOptions {
 }
 
 export async function serve(options: ServeOptions = {}) {
-  const port = options.port !== undefined ? options.port : 3000
+  const port = options.port !== undefined ? options.port : 5000
   const site = await resolveConfig(options.root)
 
   const server = http.createServer(sirv(site.outDir, { dev: true, etag: true }))
