481c4513d78450951add6177824b7996b89ae034
Matias Capeletto <matias.capeletto@gmail.com>
2021-01-22 15:47:08 +0100

    feat: $lang and $localePath globals (#166) (#167)
    close #166 
    
    Co-authored-by: Kia King Ishii <kia.king.08@gmail.com>

--- src/client/app/mixin.ts
+++ src/client/app/mixin.ts
@@ -42,6 +42,25 @@ export function mixinGlobalComputed(
       }
     },
 
+    $lang: {
+      get() {
+        return siteByRoute.value.lang
+      }
+    },
+
+    $localePath: {
+      get() {
+        const { locales } = site.value
+        const { lang } = siteByRoute.value
+
+        const path = Object.keys(locales).find(
+          (lp) => locales[lp].lang === lang
+        )
+
+        return (locales && path) || '/'
+      }
+    },
+
     $title: {
       get() {
         return page.value.title
