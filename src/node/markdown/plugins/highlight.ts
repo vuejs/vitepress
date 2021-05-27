0091f62b759c97f7b80b744434f5b657b109e1da
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-27 16:21:15 +0800

    Restore highlight.ts

--- src/node/markdown/plugins/highlight.ts
+++ src/node/markdown/plugins/highlight.ts
@@ -17,30 +17,21 @@ export const highlight = (str: string, lang: string) => {
   if (!lang) {
     return wrap(str, 'text')
   }
-
   lang = lang.toLowerCase()
   const rawLang = lang
-
   if (lang === 'vue' || lang === 'html') {
     lang = 'markup'
   }
-
   if (lang === 'md') {
     lang = 'markdown'
   }
-
   if (lang === 'ts') {
     lang = 'typescript'
   }
-
   if (lang === 'py') {
     lang = 'python'
   }
-
-  if (prism.languages[lang]) {
-    const code = prism.highlight(str, prism.languages[lang], lang)
-    return wrap(code, rawLang)
-  } else {
+  if (!prism.languages[lang]) {
     try {
       loadLanguages([lang])
     } catch (e) {
@@ -50,6 +41,10 @@ export const highlight = (str: string, lang: string) => {
         )
       )
     }
-    return wrap(str, 'text')
   }
+  if (prism.languages[lang]) {
+    const code = prism.highlight(str, prism.languages[lang], lang)
+    return wrap(code, rawLang)
+  }
+  return wrap(str, 'text')
 }
