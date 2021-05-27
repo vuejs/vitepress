b940397cd0e5135e7433bac6fc99da8553915053
heny <1623246663@qq.com>
2021-05-27 16:19:26 +0800

    fix: chinese filenames can't build (#217) (#262)

--- src/client/app/utils.ts
+++ src/client/app/utils.ts
@@ -14,6 +14,7 @@ export function joinPath(base: string, path: string): string {
  */
 export function pathToFile(path: string): string {
   let pagePath = path.replace(/\.html$/, '')
+  pagePath = decodeURIComponent(pagePath)
   if (pagePath.endsWith('/')) {
     pagePath += 'index'
   }
