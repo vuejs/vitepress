3ca3548e37520c3aa3e0de5511fdf0e4fb430c96
Chen Kai <k_0520@qq.com>
2021-11-29 16:02:08 +0800

    docs: update docsBranch (#452)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ export default {
   themeConfig: {
     repo: 'vuejs/vitepress',
     docsDir: 'docs',
-
+    docsBranch: 'main',
     editLinks: true,
     editLinkText: 'Edit this page on GitHub',
     lastUpdated: 'Last Updated',
