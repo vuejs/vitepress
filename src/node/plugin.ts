abb1b578cdedf184ae386ce455e60a23672adfcb
Evan You <yyx990803@gmail.com>
2021-12-05 23:29:41 +0800

    fix: improve createStaticVNode match for rollup codegen compat

--- src/node/plugin.ts
+++ src/node/plugin.ts
@@ -11,7 +11,7 @@ import { OutputAsset, OutputChunk } from 'rollup'
 
 const hashRE = /\.(\w+)\.js$/
 const staticInjectMarkerRE =
-  /\b(const _hoisted_\d+ = \/\*#__PURE__\*\/createStaticVNode)\("(.*)", (\d+)\)/g
+  /\b(const _hoisted_\d+ = \/\*(?:#|@)__PURE__\*\/\s*createStaticVNode)\("(.*)", (\d+)\)/g
 const staticStripRE = /__VP_STATIC_START__.*?__VP_STATIC_END__/g
 const staticRestoreRE = /__VP_STATIC_(START|END)__/g
 
