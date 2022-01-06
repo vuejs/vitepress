fb6a4ad3e008af9ce4393fb3ca37645f4efba951
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2022-01-06 10:06:46 +0630

    fix: remove `.` for mjs in `supportedConfigExtensions` (#447)

--- src/node/config.ts
+++ src/node/config.ts
@@ -145,7 +145,7 @@ export async function resolveConfig(
   return config
 }
 
-const supportedConfigExtensions = ['js', 'ts', '.mjs', 'mts']
+const supportedConfigExtensions = ['js', 'ts', 'mjs', 'mts']
 
 async function resolveUserConfig(
   root: string,
