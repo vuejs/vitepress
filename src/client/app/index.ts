37c800a2ca7c90dbc9447a20dd31abb81af1dfc5
Georges Gomes <georges.gomes@gmail.com>
2022-03-25 17:06:15 +0100

    Re-enable update of tags

--- src/client/app/index.ts
+++ src/client/app/index.ts
@@ -10,6 +10,7 @@ import {
 import { inBrowser, pathToFile } from './utils'
 import { Router, RouterSymbol, createRouter } from './router'
 import { siteDataRef, useData } from './data'
+import { useUpdateHead } from './composables/head'
 import Theme from '/@theme/index'
 import { usePrefetch } from './composables/preFetch'
 import { dataSymbol, initData } from './data'
@@ -53,6 +54,11 @@ export function createApp() {
 
   const data = initData(router.route)
   app.provide(dataSymbol, data)
+
+  if (inBrowser) {
+    // dynamically update head tags
+    useUpdateHead(router.route, data.site)
+  }
 
   // install global components
   app.component('Content', Content)
