a369973f9bd6875d5d4d9422e3ec0c8ee4c656f3
Percy Ma <kecrily@gmail.com>
2022-05-23 14:04:19 +0800

    fix(theme): show blur according to sidebar status

--- src/client/theme-default/components/VPNav.vue
+++ src/client/theme-default/components/VPNav.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { provide } from 'vue'
+import { useData } from 'vitepress'
 import { useNav } from '../composables/nav'
 import VPNavBar from './VPNavBar.vue'
 import VPNavScreen from './VPNavScreen.vue'
+import { useSidebar } from '../composables/sidebar'
 
+const { frontmatter } = useData()
+const { hasSidebar } = useSidebar()
 const { isScreenOpen, closeScreen, toggleScreen } = useNav()
 
 provide('close-screen', closeScreen)
 </script>
 
 <template>
-  <header class="VPNav">
+  <header class="VPNav" :class="{ 'no-sidebar' : !hasSidebar }">
     <VPNavBar :is-screen-open="isScreenOpen" @toggle-screen="toggleScreen" />
     <VPNavScreen :open="isScreenOpen" />
   </header>
@@ -28,6 +32,11 @@ provide('close-screen', closeScreen)
 @media (min-width: 960px) {
   .VPNav {
     position: fixed;
+  }
+}
+
+@media (min-width: 960px) {
+  .no-sidebar {
     backdrop-filter: saturate(50%) blur(8px);
   }
 }
