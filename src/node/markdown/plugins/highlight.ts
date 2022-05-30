032abf1b19d2a3b7be9f2188ba4d5da9d1fc8913
Kia Ishii <kia.king.08@gmail.com>
2022-05-30 16:02:26 +0900

    refactor: change back default theme to palenight

--- src/node/markdown/plugins/highlight.ts
+++ src/node/markdown/plugins/highlight.ts
@@ -1,9 +1,7 @@
 import { getHighlighter } from 'shiki'
 import type { ThemeOptions } from '../markdown'
 
-export async function highlight(
-  theme: ThemeOptions = { light: 'vitesse-light', dark: 'vitesse-dark' }
-) {
+export async function highlight(theme: ThemeOptions = 'material-palenight') {
   const themes = typeof theme === 'string' ? [theme] : [theme.dark, theme.light]
   const highlighter = await getHighlighter({ themes })
   const preRE = /^<pre.*?>/
@@ -15,14 +13,16 @@ export async function highlight(
       return highlighter
         .codeToHtml(str, { lang, theme })
         .replace(preRE, '<pre v-pre>')
-    } else {
-      const dark = highlighter
-        .codeToHtml(str, { lang, theme: theme.dark })
-        .replace(preRE, '<pre v-pre class="vp-code-dark">')
-      const light = highlighter
-        .codeToHtml(str, { lang, theme: theme.light })
-        .replace(preRE, '<pre v-pre class="vp-code-light">')
-      return dark + light
     }
+
+    const dark = highlighter
+      .codeToHtml(str, { lang, theme: theme.dark })
+      .replace(preRE, '<pre v-pre class="vp-code-dark">')
+
+    const light = highlighter
+      .codeToHtml(str, { lang, theme: theme.light })
+      .replace(preRE, '<pre v-pre class="vp-code-light">')
+
+    return dark + light
   }
 }
