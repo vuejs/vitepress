77fdf39a11982d71bf218d027c2abd7f73170e6d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-06-14 23:55:23 +0530

    fix: improve `copyToClipboard` function

--- src/client/theme-default/composables/copy-code.ts
+++ src/client/theme-default/composables/copy-code.ts
@@ -1,25 +1,6 @@
 import { nextTick, watch } from 'vue'
 import { inBrowser, useData } from 'vitepress'
 
-let copyToClipboard: (text: string) => Promise<void>
-
-if (navigator.clipboard) {
-  copyToClipboard = (text: string) => navigator.clipboard.writeText(text)
-} else {
-  copyToClipboard = async (text: string) => {
-    const tmp = document.createElement('TEXTAREA') as HTMLTextAreaElement
-    const activeElement = document.activeElement as HTMLElement
-    
-    tmp.value = text
-    document.body.appendChild(tmp)
-    tmp.select()
-    document.execCommand('copy')
-    document.body.removeChild(tmp)
-    
-    activeElement?.focus()
-  }
-}
-
 export function useCopyCode() {
   const { page } = useData()
 
@@ -37,6 +18,51 @@ export function useCopyCode() {
       },
       { immediate: true, flush: 'post' }
     )
+}
+
+async function copyToClipboard(text: string) {
+  try {
+    return navigator.clipboard.writeText(text)
+  } catch {
+    const element = document.createElement('textarea')
+    const previouslyFocusedElement = document.activeElement
+
+    element.value = text
+
+    // Prevent keyboard from showing on mobile
+    element.setAttribute('readonly', '')
+
+    element.style.contain = 'strict'
+    element.style.position = 'absolute'
+    element.style.left = '-9999px'
+    element.style.fontSize = '12pt' // Prevent zooming on iOS
+
+    const selection = document.getSelection()
+    const originalRange = selection
+      ? selection.rangeCount > 0 && selection.getRangeAt(0)
+      : null
+
+    document.body.append(element)
+    element.select()
+
+    // Explicit selection workaround for iOS
+    element.selectionStart = 0
+    element.selectionEnd = text.length
+
+    document.execCommand('copy')
+
+    element.remove()
+
+    if (originalRange) {
+      selection!.removeAllRanges() // originalRange can't be truthy when selection is falsy
+      selection!.addRange(originalRange)
+    }
+
+    // Get the focus back on the previously focused element, if any
+    if (previouslyFocusedElement) {
+      ;(previouslyFocusedElement as HTMLElement).focus()
+    }
+  }
 }
 
 function handleElement(el: HTMLElement) {
