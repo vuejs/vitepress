75ca9e4302c65e3bcc9518f7df928318380f6cf6
Shinigami <chrissi92@hotmail.de>
2022-06-20 07:25:40 +0200

    fix(theme): copy button has wrong tag closing syntax (#816)

--- src/node/markdown/plugins/preWrapper.ts
+++ src/node/markdown/plugins/preWrapper.ts
@@ -15,6 +15,6 @@ export const preWrapperPlugin = (md: MarkdownIt) => {
     const [tokens, idx] = args
     const token = tokens[idx]
     const rawCode = fence(...args)
-    return `<div class="language-${token.info.trim()}"><span class="copy" />${rawCode}</div>`
+    return `<div class="language-${token.info.trim()}"><span class="copy"></span>${rawCode}</div>`
   }
 }
