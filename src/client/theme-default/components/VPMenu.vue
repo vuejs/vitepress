a7c70c6d982b99d5d5254b97dad0feb580ce3c1c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-07-14 22:52:39 +0530

    fix: don't show scrollbar on menu flyouts if not necessary

--- src/client/theme-default/components/VPMenu.vue
+++ src/client/theme-default/components/VPMenu.vue
@@ -30,7 +30,7 @@ defineProps<{
   box-shadow: var(--vp-shadow-3);
   transition: background-color 0.5s;
   max-height:calc(100vh - var(--vp-nav-height-desktop));
-  overflow-y: scroll;
+  overflow-y: auto;
 }
 
 .dark .VPMenu {
