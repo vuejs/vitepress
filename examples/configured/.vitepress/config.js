bc47197f7b7c20014b322877edcef56e1374c012
fi3ework <fi3ework@gmail.com>
2022-07-27 00:10:55 +0800

    test: add E2E tests (#1012)

---
+++ examples/configured/.vitepress/config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from '../../../src/node'
+
+export default defineConfig({
+  title: 'Configured Example',
+  description: 'Example of configured options of VitePress',
+  themeConfig: {
+    sidebar: {
+      '/': [
+        {
+          text: 'Frontmatter',
+          collapsible: true,
+          items: [
+            {
+              text: 'Multiple levels outline',
+              link: '/frontmatter/multiple-levels-outline'
+            }
+          ]
+        }
+      ]
+    }
+  }
+})
