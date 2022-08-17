d7d11e744aaff6544c1e4ca5fe0140c3708bcba8
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-08-17 17:32:45 +0530

    fix(types): explicitly re-export to resolve ambiguities

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,35 @@
-export * from '../dist/node/index.js'
-export * from '../dist/client/index.js'
+export * from './shared.js'
+
+export {
+  Content,
+  EnhanceAppContext,
+  Route,
+  Router,
+  Theme,
+  VitePressData,
+  inBrowser,
+  useData,
+  useRoute,
+  useRouter,
+  withBase
+} from '../dist/client/index.js'
+
+export {
+  MarkdownOptions,
+  MarkdownParsedData,
+  MarkdownRenderer,
+  RawConfigExports,
+  ServeOptions,
+  SiteConfig,
+  ThemeOptions,
+  UserConfig,
+  build,
+  createMarkdownRenderer,
+  createServer,
+  defineConfig,
+  defineConfigWithTheme,
+  resolveConfig,
+  resolveSiteData,
+  resolveSiteDataByRoute,
+  serve
+} from '../dist/node/index.js'
