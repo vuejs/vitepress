71ad5ca7fe6134050aa3822320d546b8addf7063
7nohe <7nohe.urata@gmail.com>
2022-10-09 23:12:06 +0900

    feat(theme): allow specifying common alt for logo

--- src/client/theme-default/components/VPImage.vue
+++ src/client/theme-default/components/VPImage.vue
@@ -4,6 +4,7 @@ import { withBase } from 'vitepress'
 
 defineProps<{
   image: DefaultTheme.ThemeableImage
+  alt?: string
 }>()
 </script>
 
@@ -20,11 +21,11 @@ export default {
       class="VPImage"
       v-bind="typeof image === 'string' ? $attrs : { ...image, ...$attrs }"
       :src="withBase(typeof image === 'string' ? image : image.src)"
-      :alt="typeof image === 'string' ? '' : (image.alt || '')"
+      :alt="alt ?? (typeof image === 'string' ? '' : (image.alt || ''))"
     />
     <template v-else>
-      <VPImage class="dark" :image="image.dark" v-bind="$attrs" />
-      <VPImage class="light" :image="image.light" v-bind="$attrs" />
+      <VPImage class="dark" :image="image.dark" :alt="typeof image.dark === 'string' ? image.alt : image.dark.alt" v-bind="$attrs" />
+      <VPImage class="light" :image="image.light" :alt="typeof image.light === 'string' ? image.alt : image.light.alt" v-bind="$attrs" />
     </template>
   </template>
 </template>
