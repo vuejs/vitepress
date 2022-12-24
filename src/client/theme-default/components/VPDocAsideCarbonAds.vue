0e306255526d0134a0556923c90d27da3476fc06
Eduardo San Martin Morote <posva13@gmail.com>
2022-12-24 18:21:51 +0100

    fix: refresh ads per page navigation

--- src/client/theme-default/components/VPDocAsideCarbonAds.vue
+++ src/client/theme-default/components/VPDocAsideCarbonAds.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import { useData } from 'vitepress';
 import { defineAsyncComponent } from 'vue'
 
 const VPCarbonAds = __CARBON__
   ? defineAsyncComponent(() => import('./VPCarbonAds.vue'))
   : () => null
+
+const { page } = useData()
 </script>
 
 <template>
   <div class="VPDocAsideCarbonAds">
-    <VPCarbonAds />
+    <VPCarbonAds :key="page.relativePath" />
   </div>
 </template>
