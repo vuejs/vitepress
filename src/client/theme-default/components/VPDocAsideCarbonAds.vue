759c7fbbbfb18227e449ddb5c20bed86c6620856
Eduardo San Martin Morote <posva13@gmail.com>
2023-01-02 15:43:54 +0100

    refactor: use refresh method

--- src/client/theme-default/components/VPDocAsideCarbonAds.vue
+++ src/client/theme-default/components/VPDocAsideCarbonAds.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { useData } from 'vitepress';
 import { defineAsyncComponent } from 'vue'
 
 const VPCarbonAds = __CARBON__
   ? defineAsyncComponent(() => import('./VPCarbonAds.vue'))
   : () => null
 
-const { page } = useData()
 </script>
 
 <template>
   <div class="VPDocAsideCarbonAds">
-    <VPCarbonAds :key="page.relativePath" />
+    <VPCarbonAds />
   </div>
 </template>
