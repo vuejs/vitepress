b8a5e8e5b24b025c9a5e4850b72296f726ae71e5
Markus <markus@keck.bayern>
2023-02-25 14:10:52 +0100

    feat(theme): make prev/next links changeable (#1972)
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>

--- src/client/theme-default/components/VPDocFooter.vue
+++ src/client/theme-default/components/VPDocFooter.vue
@@ -39,15 +39,15 @@ const showFooter = computed(() => {
       </div>
     </div>
 
-    <div v-if="control.prev || control.next" class="prev-next">
+    <div v-if="control.prev?.link || control.next?.link" class="prev-next">
       <div class="pager">
-        <a v-if="control.prev" class="pager-link prev" :href="normalizeLink(control.prev.link)">
+        <a v-if="control.prev?.link" class="pager-link prev" :href="normalizeLink(control.prev.link)">
           <span class="desc" v-html="theme.docFooter?.prev || 'Previous page'"></span>
           <span class="title" v-html="control.prev.text"></span>
         </a>
       </div>
-      <div class="pager" :class="{ 'has-prev': control.prev }">
-        <a v-if="control.next" class="pager-link next" :href="normalizeLink(control.next.link)">
+      <div class="pager" :class="{ 'has-prev': control.prev?.link }">
+        <a v-if="control.next?.link" class="pager-link next" :href="normalizeLink(control.next.link)">
           <span class="desc" v-html="theme.docFooter?.next || 'Next page'"></span>
           <span class="title" v-html="control.next.text"></span>
         </a>
