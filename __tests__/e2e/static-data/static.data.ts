e29b6a051e89e23945e2acfdfca7057978929715
Evan You <yyx990803@gmail.com>
2023-03-07 20:18:40 +0800

    feat(data-loader): pass watched files into load()

--- __tests__/e2e/static-data/static.data.ts
+++ __tests__/e2e/static-data/static.data.ts
@@ -1,21 +1,17 @@
 import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 type Data = Record<string, boolean>[]
 export declare const data: Data
 
 export default {
   watch: ['./data/*'],
-  async load(): Promise<Data> {
+  async load(files: string[]): Promise<Data> {
     const foo = fs.readFileSync(
-      path.resolve(dirname, './data/foo.json'),
+      files.find((f) => f.endsWith('foo.json'))!,
       'utf-8'
     )
     const bar = fs.readFileSync(
-      path.resolve(dirname, './data/bar.json'),
+      files.find((f) => f.endsWith('bar.json'))!,
       'utf-8'
     )
     return [JSON.parse(foo), JSON.parse(bar)]
