dfe81438c78af0cfc518b364a6cc6bc0ecdfb6f3
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2023-03-15 08:57:25 +0000

    Adds suggestion

--- src/client/app/composables/codeGroups.ts
+++ src/client/app/composables/codeGroups.ts
@@ -10,13 +10,10 @@ export function useCodeGroups() {
         const group = el.parentElement?.parentElement
         const i = Array.from(group?.querySelectorAll('input') || []).indexOf(el)
 
-        // This filters out shiki twoslash divs with the `language-id` className.
-        const filtered = Array.from(
-          group?.querySelectorAll('div[class*="language-"]') || []
-        ).filter((val) => !val.className.match('language-id'))
-
         const current = group?.querySelector('div[class*="language-"].active')
-        const next = filtered?.[i]
+        const next = group?.querySelectorAll(
+          'div[class*="language-"]:not(.language-id)'
+        )?.[i]
 
         if (current && next && current !== next) {
           current.classList.remove('active')
