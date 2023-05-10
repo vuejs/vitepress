b31933fbdd7aabfe080234407153aefa8f6a3f30
烽宁 <miqilin18@gmail.com>
2023-05-10 23:28:59 +0800

    fix(build): uniform handling of windows slash in localSearchPlugin (#2358)

--- src/client/theme-default/components/VPLocalSearchBox.vue
+++ src/client/theme-default/components/VPLocalSearchBox.vue
@@ -29,7 +29,6 @@ import {
 import type { ModalTranslations } from '../../../../types/local-search'
 import { dataSymbol } from '../../app/data'
 import { pathToFile } from '../../app/utils'
-import { slash } from '../../shared'
 import { useData } from '../composables/data'
 import { createTranslate } from '../support/translation'
 
@@ -218,7 +217,7 @@ debouncedWatch(
 )
 
 async function fetchExcerpt(id: string) {
-  const file = pathToFile(slash(id.slice(0, id.indexOf('#'))))
+  const file = pathToFile(id.slice(0, id.indexOf('#')))
   try {
     return { id, mod: await import(/*@vite-ignore*/ file) }
   } catch (e) {
