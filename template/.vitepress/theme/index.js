7e39e02185f5b18da09b01bd4c132a8b50e15b07
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-06-12 21:49:08 +0530

    fix: use extends in template custom theme (#2500)

--- template/.vitepress/theme/index.js
+++ template/.vitepress/theme/index.js
@@ -13,7 +13,7 @@ import Theme from 'vitepress/theme'
 import './style.css'
 
 export default {
-  ...Theme,
+  extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
