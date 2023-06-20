cd03db803d5e6b9f04e242f7843153dded73ccb2
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-06-20 12:32:27 +0530

    revert!: sync defineConfig types with vite (#2529)
    BREAKING CHANGE: reverts the breaking changes in beta-2

--- src/node/config.ts
+++ src/node/config.ts
@@ -38,7 +38,7 @@ export type UserConfigExport<ThemeConfig> =
 /**
  * Type config helper
  */
-export function defineConfig(config: UserConfigExport<DefaultTheme.Config>) {
+export function defineConfig(config: UserConfig<DefaultTheme.Config>) {
   return config
 }
 
@@ -46,7 +46,7 @@ export function defineConfig(config: UserConfigExport<DefaultTheme.Config>) {
  * Type config helper for custom theme config
  */
 export function defineConfigWithTheme<ThemeConfig>(
-  config: UserConfigExport<ThemeConfig>
+  config: UserConfig<ThemeConfig>
 ) {
   return config
 }
