338f235a3afedee6b69bfdddd3f2a2c7f91442cc
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-07-05 11:43:16 +0530

    restrict minisearch options a bit

--- types/default-theme.d.ts
+++ types/default-theme.d.ts
@@ -1,5 +1,5 @@
-import { type ComputedRef, type Ref } from 'vue'
-import { type Options, type SearchOptions } from 'minisearch'
+import type { Options as MiniSearchOptions } from 'minisearch'
+import type { ComputedRef, Ref } from 'vue'
 import type { DocSearchProps } from './docsearch.js'
 import type { LocalSearchTranslations } from './local-search.js'
 import type { PageData } from './shared.js'
@@ -337,14 +337,18 @@ export namespace DefaultTheme {
     translations?: LocalSearchTranslations
     locales?: Record<string, Partial<Omit<LocalSearchOptions, 'locales'>>>
 
-    /**
-     * The minisearch scalable options.
-     * @see https://lucaong.github.io/minisearch/modules/_minisearch_.html#options
-     * @see https://lucaong.github.io/minisearch/modules/_minisearch_.html#searchoptions-1
-     */
     miniSearch?: {
-      options?: Omit<Options, 'fields'>
-      searchOptions?: SearchOptions
+      /**
+       * @see https://lucaong.github.io/minisearch/modules/_minisearch_.html#options
+       */
+      options?: Pick<
+        MiniSearchOptions,
+        'extractField' | 'tokenize' | 'processTerm'
+      >
+      /**
+       * @see https://lucaong.github.io/minisearch/modules/_minisearch_.html#searchoptions-1
+       */
+      searchOptions?: MiniSearchOptions['searchOptions']
     }
   }
 
