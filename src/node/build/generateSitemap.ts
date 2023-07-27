1edbf8aad252d1367b796304e58e78536e634a76
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-07-27 20:49:43 +0530

    minor tweaks

--- src/node/build/generateSitemap.ts
+++ src/node/build/generateSitemap.ts
@@ -7,31 +7,38 @@ import { task } from '../utils/task'
 
 export async function generateSitemap(siteConfig: SiteConfig) {
   if (!siteConfig.sitemap?.hostname) return
-
+  const { transformItems, ...options } = siteConfig.sitemap
   await task('generating sitemap', async () => {
-    const links = (
+    const items = (
       await Promise.all(
         siteConfig.pages.map(async (page) => {
           const url = (siteConfig.rewrites.map[page] || page)
             .replace(/(^|\/)?index.md$/, '$1')
             .replace(/\.md$/, siteConfig.cleanUrls ? '' : '.html')
-          const lastmod = (await getGitTimestamp(page)) || undefined
+          const lastmod =
+            (siteConfig.lastUpdated && (await getGitTimestamp(page))) ||
+            undefined
           return { url, lastmod }
         })
       )
     ).sort((a, b) => a.url.localeCompare(b.url))
-
-    const items = siteConfig.sitemap?.transformItems
-      ? await siteConfig.sitemap.transformItems(links)
-      : links
-
-    const sitemapStream = new SitemapStream(siteConfig.sitemap)
+    const sitemapStream = new SitemapStream({
+      xmlns: {
+        news: false,
+        xhtml: false,
+        image: false,
+        video: false,
+        ...options.xmlns
+      },
+      ...options
+    })
     const writeStream = fs.createWriteStream(
       path.join(siteConfig.outDir, 'sitemap.xml')
     )
-
     sitemapStream.pipe(writeStream)
-    items.forEach((link) => sitemapStream.write(link))
+    ;((await transformItems?.(items)) || items).forEach((item) =>
+      sitemapStream.write(item)
+    )
     sitemapStream.end()
   })
 }
