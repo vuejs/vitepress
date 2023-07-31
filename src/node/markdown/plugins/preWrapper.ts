ab14d967b191e7bd60af6db6a47a29ccc116d607
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-07-31 23:49:57 +0530

    fix: wrong lang when no lang is specified on first code block in a group

--- src/node/markdown/plugins/preWrapper.ts
+++ src/node/markdown/plugins/preWrapper.ts
@@ -9,14 +9,18 @@ export function preWrapperPlugin(md: MarkdownIt, options: Options) {
   md.renderer.rules.fence = (...args) => {
     const [tokens, idx] = args
     const token = tokens[idx]
+
     // remove title from info
     token.info = token.info.replace(/\[.*\]/, '')
 
+    const active = / active( |$)/.test(token.info) ? ' active' : ''
+    token.info = token.info.replace(/ active$/, '').replace(/ active /, ' ')
+
     const lang = extractLang(token.info)
     const rawCode = fence(...args)
-    return `<div class="language-${lang}${getAdaptiveThemeMarker(options)}${
-      / active( |$)/.test(token.info) ? ' active' : ''
-    }"><button title="Copy Code" class="copy"></button><span class="lang">${lang}</span>${rawCode}</div>`
+    return `<div class="language-${lang}${getAdaptiveThemeMarker(
+      options
+    )}${active}"><button title="Copy Code" class="copy"></button><span class="lang">${lang}</span>${rawCode}</div>`
   }
 }
 
