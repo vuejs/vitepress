a4400887fbc2a810facedef391fb3bac14e06237
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-09-10 19:44:10 +0530

    minify

--- src/node/plugin.ts
+++ src/node/plugin.ts
@@ -266,13 +266,7 @@ export async function createVitePressPlugin(
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="description" content="">
-    <noscript>
-      <style>
-        .js-required {
-          display: none !important;
-        }
-      </style>
-    </noscript>
+    <noscript><style>.js-required{display:none!important}</style></noscript>
   </head>
   <body>
     <div id="app"></div>
