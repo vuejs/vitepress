e4b50260607193af8cf67c07e81833e63f696067
Neo <558368+wangxiao@users.noreply.github.com>
2023-09-15 22:13:39 +0800

    fix:  Fix the in-document advertising styles.

--- docs/.vitepress/theme/AdComponent.vue
+++ docs/.vitepress/theme/AdComponent.vue
@@ -3,26 +3,25 @@ import icon from './documate.vue'
 </script>
 
 <template>
-  <a class="ad-component" target="_blank" href="https://documate.site/">
-    <icon class="icon"/>
-    <span>Embed AI chat in your VitePress using your own content. Open-source.</span>
-    <span class="cta">Try now →</span>
-  </a>
+  <div class="ad-component">
+    <a target="_blank" href="https://documate.site/">
+      <icon class="icon"/>
+      <span>Embed AI chat in your VitePress using your own content. Open-source.</span>
+      <span class="cta">Try now →</span>
+    </a>
+  </div>
 </template>
 
 <style scoped>
 .ad-component {
   margin-bottom: 2rem;
-  padding: .65rem 1rem;
+  padding: .5rem .85rem;
   border: 1px solid var(--vp-c-divider);
   border-radius: 4px;
-  text-align: center;
-  display: flex;
-  align-items: center;
   text-decoration: none;
   color: var(--vp-c-text-2);
   transition: color 0.4s ease-in-out;
-  font-size: 0.9rem;
+  font-size: 0.8rem;
 }
 
 .ad-component:hover {
@@ -30,13 +29,14 @@ import icon from './documate.vue'
 }
 
 .ad-component .icon {
-  width: 1rem;
-  height: 1rem;
-  margin-right: 0.45rem;
+  display: inline-block;
+  width: .75rem;
+  height: .75rem;
+  margin-right: 0.3rem;
 }
 
 .ad-component .cta {
-  margin-left: 0.2rem;
+  margin-left: 0.3rem;
   text-decoration: underline;
   text-underline-offset: 2px;
 }
