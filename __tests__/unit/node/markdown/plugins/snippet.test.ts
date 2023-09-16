2189b85b323ab00ac5dadd2c57b6edc2b20dbf35
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-09-17 01:50:59 +0530

    add more tests

--- __tests__/unit/node/markdown/plugins/snippet.test.ts
+++ __tests__/unit/node/markdown/plugins/snippet.test.ts
@@ -7,7 +7,7 @@ const removeEmptyKeys = <T extends Record<string, unknown>>(obj: T) => {
 }
 
 /* prettier-ignore */
-const rawPathTokenMap: [string, Partial<{ filepath: string, extension: string, region: string, lines: string, lang: string, title: string}>][] = [
+const rawPathTokenMap: [string, Partial<{ filepath: string, extension: string, title: string, region: string, lines: string, lang: string }>][] = [
   ['/path/to/file.extension', { filepath: '/path/to/file.extension', extension: 'extension', title: 'file.extension' }],
   ['./path/to/file.extension', { filepath: './path/to/file.extension', extension: 'extension', title: 'file.extension' }],
   ['/path to/file.extension', { filepath: '/path to/file.extension', extension: 'extension', title: 'file.extension' }],
@@ -23,7 +23,17 @@ const rawPathTokenMap: [string, Partial<{ filepath: string, extension: string, r
   ['/path.to/file', { filepath: '/path.to/file', title: 'file' }],
   ['./path.to/file', { filepath: './path.to/file', title: 'file' }],
   ['/path .to/file', { filepath: '/path .to/file', title: 'file' }],
-  ['./path .to/file', { filepath: './path .to/file', title: 'file' }]
+  ['./path .to/file', { filepath: './path .to/file', title: 'file' }],
+  ['/path/to/file.extension#region', { filepath: '/path/to/file.extension', extension: 'extension', title: 'file.extension', region: '#region' }],
+  ['./path/to/file.extension {c#}', { filepath: './path/to/file.extension', extension: 'extension', title: 'file.extension', lang: 'c#' }],
+  ['/path to/file.extension {1,2,4-6}', { filepath: '/path to/file.extension', extension: 'extension', title: 'file.extension', lines: '1,2,4-6' }],
+  ['/path to/file.extension {1,2,4-6 c#}', { filepath: '/path to/file.extension', extension: 'extension', title: 'file.extension', lines: '1,2,4-6', lang: 'c#' }],
+  ['/path.to/file.extension [title]', { filepath: '/path.to/file.extension', extension: 'extension', title: 'title' }],
+  ['./path.to/file.extension#region {c#}', { filepath: './path.to/file.extension', extension: 'extension', title: 'file.extension', region: '#region', lang: 'c#' }],
+  ['/path/to/file#region {1,2,4-6}', { filepath: '/path/to/file', title: 'file', region: '#region', lines: '1,2,4-6' }],
+  ['./path/to/file#region {1,2,4-6 c#}', { filepath: './path/to/file', title: 'file', region: '#region', lines: '1,2,4-6', lang: 'c#' }],
+  ['/path to/file {1,2,4-6 c#} [title]', { filepath: '/path to/file', title: 'title', lines: '1,2,4-6', lang: 'c#' }],
+  ['./path to/file#region {1,2,4-6 c#} [title]', { filepath: './path to/file', title: 'title', region: '#region', lines: '1,2,4-6', lang: 'c#' }],
 ]
 
 describe('node/markdown/plugins/snippet', () => {
