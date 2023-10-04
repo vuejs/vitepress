2bf0d0b9541aa6ef4607d00000753344ea2a40b9
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-05 00:48:06 +0530

    docs: fix safari favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,32 +23,16 @@ export default defineConfig({
     }
   },
 
+  /* prettier-ignore */
   head: [
-    ['link', { rel: 'icon', href: '/vitepress-logo-mini.svg' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/vitepress-logo-mini.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/vitepress-logo-mini.png' }],
     ['meta', { name: 'theme-color', content: '#5f67ee' }],
     ['meta', { name: 'og:type', content: 'website' }],
     ['meta', { name: 'og:locale', content: 'en' }],
     ['meta', { name: 'og:site_name', content: 'VitePress' }],
-    [
-      'meta',
-      { name: 'og:image', content: 'https://vitepress.dev/vitepress-og.jpg' }
-    ],
-    [
-      'meta',
-      {
-        name: 'twitter:image',
-        content: 'https://vitepress.dev/vitepress-og.jpg'
-      }
-    ],
-    [
-      'script',
-      {
-        src: 'https://cdn.usefathom.com/script.js',
-        'data-site': 'AZBRSFGG',
-        'data-spa': 'auto',
-        defer: ''
-      }
-    ]
+    ['meta', { name: 'og:image', content: 'https://vitepress.dev/vitepress-og.jpg' }],
+    ['script', { src: 'https://cdn.usefathom.com/script.js', 'data-site': 'AZBRSFGG', 'data-spa': 'auto', defer: '' }]
   ],
 
   themeConfig: {
