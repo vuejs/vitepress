2de5096ffb393ad8808a194cb7f640ec918751c5
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-22 16:43:08 +0530

    chore: remove extra lines

--- template/.vitepress/theme/index.js
+++ template/.vitepress/theme/index.js
@@ -1,22 +1,22 @@
 // https://vitepress.dev/guide/custom-theme
-<% if (!defaultTheme) { %>import Layout from './Layout.vue'
-<% if (useTs) { %>import type { Theme } from 'vitepress'<% } %>
+<% if (!defaultTheme) { %>import Layout from './Layout.vue'<% if (useTs) { %>
+import type { Theme } from 'vitepress'<% } %>
 import './style.css'
 
-<% if (!useTs) { %>/** @type {import('vitepress').Theme} */<% } %>
-export default {
+<% if (!useTs) { %>/** @type {import('vitepress').Theme} */
+<% } %>export default {
   Layout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
 }<% if (useTs) { %> satisfies Theme<% } %>
-<% } else { %>import { h } from 'vue'
-<% if (useTs) { %>import type { Theme } from 'vitepress'<% } %>
+<% } else { %>import { h } from 'vue'<% if (useTs) { %>
+import type { Theme } from 'vitepress'<% } %>
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
-<% if (!useTs) { %>/** @type {import('vitepress').Theme} */<% } %>
-export default {
+<% if (!useTs) { %>/** @type {import('vitepress').Theme} */
+<% } %>export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
