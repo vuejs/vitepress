d58d3b44b439e566f743c610c8c5a7d0015c0396
userquin <userquin@gmail.com>
2023-10-22 18:56:47 +0200

    chore: handle fm aside and outline for outline

--- src/client/theme-default/composables/screen-only.ts
+++ src/client/theme-default/composables/screen-only.ts
@@ -5,11 +5,13 @@ import { computed } from 'vue'
 export function useScreenOnly(entry: keyof DefaultTheme.PrintOptions) {
   const { theme, frontmatter } = useData()
 
-  return computed(
-    () =>
-      frontmatter.value[entry] === false ||
-      theme.value.print === false ||
-      (typeof theme.value.print === 'object' &&
-        theme.value.print[entry] === false)
+  // we have aside and outline in frontmatter, we will check for both for outline
+  return computed(() =>
+    entry === 'outline'
+      ? frontmatter.value.aside === false || frontmatter.value.outline === false
+      : frontmatter.value[entry] === false ||
+        theme.value.print === false ||
+        (typeof theme.value.print === 'object' &&
+          theme.value.print[entry] === false)
   )
 }
