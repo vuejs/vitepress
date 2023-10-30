2276c1d4dac547bb09015fcd0df73825b32c5fad
YiZhi <yizhi0911@gmail.com>
2023-10-30 11:02:33 +0800

    fix(theme): theme switch is not hidden on force-dark (#3155)

--- src/client/theme-default/components/VPNavScreenAppearance.vue
+++ src/client/theme-default/components/VPNavScreenAppearance.vue
@@ -6,7 +6,7 @@ const { site, theme } = useData()
 </script>
 
 <template>
-  <div v-if="site.appearance" class="VPNavScreenAppearance">
+  <div v-if="site.appearance && site.appearance !== 'force-dark'" class="VPNavScreenAppearance">
     <p class="text">
       {{ theme.darkModeSwitchLabel || 'Appearance' }}
     </p>
