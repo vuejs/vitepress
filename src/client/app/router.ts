0d6f340ddd3e3687b675fc693faf0f886052cc59
babakfp <babak.bxf@gmail.com>
2023-12-09 06:28:15 -0800

    better warn message

--- src/client/app/router.ts
+++ src/client/app/router.ts
@@ -213,7 +213,7 @@ export function createRouter(
         if (!failedLangFallbackLang) return
         if (!langNames.includes(failedLangFallbackLang)) {
           console.warn(
-            `Invalid value received in "VitePress Config" > "locales.${failedLang}.fallback"! "${failedLangFallbackLang}" is not a valid value.`
+            `Invalid value received in "VitePress Config" > "locales.${failedLang}.fallback". "${failedLangFallbackLang}" is not a valid value.`
           )
           return
         }
@@ -236,7 +236,7 @@ export function createRouter(
         if (!fallbackLang) return
         if (!langNames.includes(fallbackLang)) {
           console.warn(
-            `Invalid value received in "VitePress Config" > "locales.root.fallback"! "${fallbackLang}" is not a valid value.`
+            `Invalid value received in "VitePress Config" > "locales.root.fallback". "${fallbackLang}" is not a valid value.`
           )
           return
         }
