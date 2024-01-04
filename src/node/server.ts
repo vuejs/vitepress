b2a9eb6bfa3f7d26cf2aeb278a348a7900e34e11
Waleed Khaled <waleed1kh@outlook.com>
2024-01-04 21:16:43 +0400

    fix: server restart config load issue

--- src/node/server.ts
+++ src/node/server.ts
@@ -13,7 +13,6 @@ export async function createServer(
     'development',
     serverOptions.configFile
   )
-  delete serverOptions.configFile
 
   if (serverOptions.base) {
     config.site.base = serverOptions.base
