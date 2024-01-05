32ead8a75a0eccf4cf41a4961c1b14fc47b4a1cb
Yuxuan Zhang <github@z-yx.cc>
2024-01-05 12:11:27 -0500

    fix(cli/shortcuts): CTRL-C should be considered normal exit

--- src/node/shortcuts.ts
+++ src/node/shortcuts.ts
@@ -33,8 +33,10 @@ export function bindShortcuts(
   const onInput = async (input: string) => {
     // ctrl+c or ctrl+d
     if (input === '\x03' || input === '\x04') {
-      await server.close().finally(() => process.exit(1))
-      return
+      await server
+        .close()
+        .catch(() => (process.exitCode = 1))
+        .finally(() => process.exit())
     }
 
     if (actionRunning) return
