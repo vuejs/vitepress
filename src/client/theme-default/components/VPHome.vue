30c665b29d419c1b5057222743e5394e7260b809
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-02-02 01:13:03 +0530

    chore: bump deps

--- src/client/theme-default/components/VPHome.vue
+++ src/client/theme-default/components/VPHome.vue
@@ -7,7 +7,10 @@ import VPHomeFeatures from './VPHomeFeatures.vue'
   <div class="VPHome">
     <slot name="home-hero-before" />
     <VPHomeHero>
+      <template #home-hero-info-before><slot name="home-hero-info-before" /></template>
       <template #home-hero-info><slot name="home-hero-info" /></template>
+      <template #home-hero-info-after><slot name="home-hero-info-after" /></template>
+      <template #home-hero-actions-after><slot name="home-hero-actions-after" /></template>
       <template #home-hero-image><slot name="home-hero-image" /></template>
     </VPHomeHero>
     <slot name="home-hero-after" />
