27dd0ba4ae288a37810c1a14c270f533decf03c5
Jacob Hummer <jcbhmr@outlook.com>
2024-02-07 16:42:36 -0600

    Create VPHomeContent.vue

---
+++ src/client/theme-default/components/VPHomeContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="vp-doc container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: auto;
+  width: 100%;
+  max-width: 1280px;
+  padding: 0 24px;
+}
+
+@media (min-width: 640px) {
+  .container {
+    padding: 0 48px;
+  }
+}
+
+@media (min-width: 960px) {
+  .container {
+    width: 100%;
+    padding: 0 64px;
+  }
+}
+</style>
