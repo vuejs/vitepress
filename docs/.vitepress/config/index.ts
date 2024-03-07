2b40266dacc1145542ec67937a54304ead1b7a67
viniciusdeliz <vinicius.deliz@gmail.com>
2024-03-07 04:30:37 -0300

    docs(pt): Create Portuguese VitePress translation (#3531)
    Co-authored-by: Savio Nascimento <savionascimentodev@gmail.com>
    Co-authored-by: Xavi Lee <xavilee2002@outlook.com>
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vitepress'
 import { shared } from './shared'
 import { en } from './en'
 import { zh } from './zh'
+import { pt } from './pt'
 
 export default defineConfig({
   ...shared,
   locales: {
     root: { label: 'English', ...en },
-    zh: { label: '简体中文', ...zh }
+    zh: { label: '简体中文', ...zh },
+    pt: { label: 'Português', ...pt }
   }
 })
