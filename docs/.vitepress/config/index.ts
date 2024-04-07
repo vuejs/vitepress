58324020c76c5f9e3ef15576083ca244f764a336
Bugo <cheetah@bk.ru>
2024-04-07 18:36:11 +0500

    docs(ru): add Russian translation (#3709)
    Co-authored-by: Ruslan Makarov <ruslan.makarov@gmail.com>

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -3,6 +3,7 @@ import { shared } from './shared'
 import { en } from './en'
 import { zh } from './zh'
 import { pt } from './pt'
+import { ru } from './ru'
 
 export default defineConfig({
   ...shared,
@@ -10,6 +11,7 @@ export default defineConfig({
     root: { label: 'English', ...en },
     zh: { label: '简体中文', ...zh },
     pt: { label: 'Português', ...pt },
+    ru: { label: 'Русский', ...ru },
     ko: { label: '한국어', lang: 'ko-KR', link: 'https://vitepress.vuejs.kr/' }
   }
 })
--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 import { search as zhSearch } from './zh'
 import { search as ptSearch } from './pt'
+import { search as ruSearch } from './ru'
 
 export const shared = defineConfig({
   title: 'VitePress',
@@ -55,7 +56,7 @@ export const shared = defineConfig({
         appId: '8J64VVRP8K',
         apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
         indexName: 'vitepress',
-        locales: { ...zhSearch, ...ptSearch }
+        locales: { ...zhSearch, ...ptSearch, ...ruSearch }
       }
     },
 
