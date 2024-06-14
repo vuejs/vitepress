590865b6e76fdb6da65534a7e6cac6c7b15944bf
xiaodong2008 <admin@xiaodong.moe>
2024-06-14 13:40:51 +0800

    update

--- src/client/theme-default/index.ts
+++ src/client/theme-default/index.ts
@@ -1,8 +1,4 @@
 import './styles/fonts.css'
 
-import VPHomeContent from './components/VPHomeContent.vue'
-
 export * from './without-fonts'
 export { default as default } from './without-fonts'
-
-export { VPHomeContent }
