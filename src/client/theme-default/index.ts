5b39fefd3b433b95090cb1ac085e2635ff7905ee
xiaodong2008 <admin@xiaodong.moe>
2024-06-14 13:32:49 +0800

    feat(theme): export VPHomeContent

--- src/client/theme-default/index.ts
+++ src/client/theme-default/index.ts
@@ -1,4 +1,8 @@
 import './styles/fonts.css'
 
+import VPHomeContent from './components/VPHomeContent.vue'
+
 export * from './without-fonts'
 export { default as default } from './without-fonts'
+
+export { VPHomeContent }
