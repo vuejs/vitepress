b5e28c82f2a5aad26f304e14d08857c41f0f8d5d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-06-29 14:20:38 +0530

    fix conditions

--- src/node/utils/processIncludes.ts
+++ src/node/utils/processIncludes.ts
@@ -1,8 +1,9 @@
 import fs from 'fs-extra'
 import matter from 'gray-matter'
 import path from 'path'
-import { slash } from '../shared'
+import c from 'picocolors'
 import { findRegion } from '../markdown/plugins/snippet'
+import { slash } from '../shared'
 
 export function processIncludes(
   srcDir: string,
@@ -11,15 +12,21 @@ export function processIncludes(
   includes: string[]
 ): string {
   const includesRE = /<!--\s*@include:\s*(.*?)\s*-->/g
-  const rangeRE = /\{(\d*),(\d*)\}$/
   const regionRE = /(#[\w-]+)/
+  const rangeRE = /\{(\d*),(\d*)\}$/
+
   return src.replace(includesRE, (m: string, m1: string) => {
     if (!m1.length) return m
 
     const range = m1.match(rangeRE)
     const region = m1.match(regionRE)
-    range && (m1 = m1.slice(0, -range[0].length))
-    region && (m1 = m1.slice(0, -region[0].length))
+
+    const hasMeta = !!(region || range)
+
+    if (hasMeta) {
+      const len = (region?.[0].length || 0) + (range?.[0].length || 0)
+      m1 = m1.slice(0, -len) // remove meta info from the include path
+    }
 
     const atPresent = m1[0] === '@'
 
@@ -32,10 +39,8 @@ export function processIncludes(
       if (region) {
         const [regionName] = region
         const lines = content.split(/\r?\n/)
-        const regionLines = findRegion(lines, regionName.replace('#', ''))
-        content = lines
-          .slice(regionLines?.start || undefined, regionLines?.end || undefined)
-          .join('\n')
+        const regionLines = findRegion(lines, regionName.slice(1))
+        content = lines.slice(regionLines?.start, regionLines?.end).join('\n')
       }
 
       if (range) {
@@ -47,13 +52,22 @@ export function processIncludes(
             endLine ? parseInt(endLine, 10) : undefined
           )
           .join('\n')
-      } else {
+      }
+
+      if (!hasMeta && path.extname(includePath) === '.md') {
         content = matter(content).content
       }
+
       includes.push(slash(includePath))
       // recursively process includes in the content
       return processIncludes(srcDir, content, includePath, includes)
+
+      //
     } catch (error) {
+      if (process.env.DEBUG) {
+        process.stderr.write(c.yellow(`\nInclude file not found: ${m1}`))
+      }
+
       return m // silently ignore error if file is not present
     }
   })
