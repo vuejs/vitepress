0fc70e2d0c29c92a1dceba035295a3fac84ffc00
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-06-30 13:13:09 +0530

    fix(theme): show filled nav bar when nav screen is open

--- src/client/theme-default/components/VPNavBar.vue
+++ src/client/theme-default/components/VPNavBar.vue
@@ -12,7 +12,7 @@ import VPNavBarSocialLinks from './VPNavBarSocialLinks.vue'
 import VPNavBarTitle from './VPNavBarTitle.vue'
 import VPNavBarTranslations from './VPNavBarTranslations.vue'
 
-defineProps<{
+const props = defineProps<{
   isScreenOpen: boolean
 }>()
 
@@ -31,6 +31,7 @@ watchPostEffect(() => {
     'has-sidebar': hasSidebar.value,
     'home': frontmatter.value.layout === 'home',
     'top': y.value === 0,
+    'screen-open': props.isScreenOpen
   }
 })
 </script>
@@ -74,7 +75,13 @@ watchPostEffect(() => {
   height: var(--vp-nav-height);
   pointer-events: none;
   white-space: nowrap;
-  transition: background-color 0.5s;
+  transition: background-color 0.25s;
+}
+
+.VPNavBar.screen-open {
+  transition: none;
+  background-color: var(--vp-nav-bg-color);
+  border-bottom: 1px solid var(--vp-c-divider);
 }
 
 .VPNavBar:not(.home) {
