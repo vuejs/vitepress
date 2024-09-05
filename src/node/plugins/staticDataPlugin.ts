cb1106d2ce67fd3c1acc798240363252d6e9c94f
Cristopher <32661241+Namchee@users.noreply.github.com>
2024-09-05 19:46:05 +0700

    chore(deps): replace fast-glob with tinyglobby (#4132)

--- src/node/plugins/staticDataPlugin.ts
+++ src/node/plugins/staticDataPlugin.ts
@@ -6,7 +6,7 @@ import {
 } from 'vite'
 import path, { dirname, resolve } from 'path'
 import { isMatch } from 'micromatch'
-import glob from 'fast-glob'
+import { glob } from 'tinyglobby'
 
 const loaderMatch = /\.data\.m?(j|t)s($|\?)/
 
@@ -98,9 +98,11 @@ export const staticDataPlugin: Plugin = {
       // load the data
       let watchedFiles
       if (watch) {
+        if (typeof watch === 'string') watch = [watch]
         watchedFiles = (
           await glob(watch, {
-            ignore: ['**/node_modules/**', '**/dist/**']
+            ignore: ['**/node_modules/**', '**/dist/**'],
+            expandDirectories: false
           })
         ).sort()
       }
