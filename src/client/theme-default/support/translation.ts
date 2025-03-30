8d4ae1ce034a5885b35cf38b78647e1c94ec95a2
Yuxuan Zhang <github@z-yx.cc>
2025-03-30 07:33:45 -0400

    revert translation.ts: remove unneeded code

--- src/client/theme-default/support/translation.ts
+++ src/client/theme-default/support/translation.ts
@@ -15,7 +15,6 @@ export function createSearchTranslate(
     const isObject = themeObject && typeof themeObject === 'object'
     const locales =
       (isObject && themeObject.locales?.[localeIndex.value]?.translations) ||
-      (isObject && themeObject?.translations) ||
       null
     const translations = (isObject && themeObject.translations) || null
 
