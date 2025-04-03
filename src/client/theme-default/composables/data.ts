f1233ab5bbe7c67217788e671afefebd549deffd
Yuxuan Zhang <github@z-yx.cc>
2025-04-03 01:33:55 -0400

    feat: default-theme-is-home-layout

--- src/client/theme-default/composables/data.ts
+++ src/client/theme-default/composables/data.ts
@@ -1,4 +1,12 @@
 import { useData as useData$ } from 'vitepress'
 import type { DefaultTheme } from 'vitepress/theme'
 
-export const useData: typeof useData$<DefaultTheme.Config> = useData$
+export function useData() {
+  const data = useData$<DefaultTheme.Config>()
+
+  // Allow an arbitrary layout to be used as the home layout
+  ;(data.frontmatter as any).isHomeLayout ??=
+    (data.frontmatter as any).layout === 'home'
+
+  return data
+}
