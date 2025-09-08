fe26488bbc2d92f6b081da4cf33ded2d70e9c371
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-09-08 14:41:44 +0530

    docs: fix incorrect lang being applied on all pages

--- docs/config.ts
+++ docs/config.ts
@@ -5,7 +5,6 @@ const require = createRequire(import.meta.url)
 const pkg = require('vitepress/package.json')
 
 export default defineAdditionalConfig({
-  lang: 'en-US',
   description: 'Vite & Vue powered static site generator.',
 
   themeConfig: {
