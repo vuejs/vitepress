9942f7358563803a8e0741885097ca3f2004c851
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2026-02-10 12:13:21 +0530

    wip

--- scripts/build.js
+++ scripts/build.js
@@ -12,56 +12,59 @@ const typesExternal = [
   /^markdown-it(?:$|\/)/
 ]
 
-await Promise.all([
-  fs.copyFile('src/shared/shared.ts', 'src/node/shared.ts'),
-  fs.copyFile('src/shared/shared.ts', 'src/client/shared.ts')
-])
-
-await build({
-  checks: { eval: false, pluginTimings: false },
-  config: false,
-  dts: { resolver: 'tsc' },
-  entry: ['src/node/index.ts', 'src/node/cli.ts'],
-  failOnWarn: true,
-  fixedExtension: false,
-  inlineOnly: false,
-  logLevel: 'warn',
-  minify: { codegen: false, compress: true, mangle: false },
-  nodeProtocol: true,
-  outDir: 'dist/node',
-  platform: 'node',
-  target: 'node20',
-  sourcemap: DEV,
-  tsconfig: 'src/node/tsconfig.json',
-  external: (id, parentId) =>
-    (parentId?.endsWith('.d.ts') && typesExternal.some((re) => re.test(id))) ||
-    undefined
-})
-
-console.log('Node build complete.')
-
-await mkdist({
-  addRelativeDeclarationExtensions: true,
-  declaration: true,
-  distDir: 'dist/client',
-  ext: 'js',
-  format: 'esm',
-  pattern: '**/*.{vue,ts,css,woff2}',
-  srcDir: 'src/client'
-})
-
-const dtsFiles = await glob('dist/client/**/*.d.ts')
-await Promise.all(
-  dtsFiles.map(async (file) => {
-    const content = await fs.readFile(file, 'utf-8')
-    const updated = content.replace(/import\s+["'][^"']+["'];\n/g, '')
-    if (updated !== content) {
-      await fs.writeFile(file, updated)
-    }
+async function buildNode() {
+  await build({
+    checks: { eval: false, pluginTimings: false },
+    config: false,
+    dts: { resolver: 'tsc' },
+    entry: ['src/node/index.ts', 'src/node/cli.ts'],
+    failOnWarn: true,
+    fixedExtension: false,
+    inlineOnly: false,
+    logLevel: 'warn',
+    minify: { codegen: false, compress: true, mangle: false },
+    nodeProtocol: true,
+    outDir: 'dist/node',
+    platform: 'node',
+    target: 'node20',
+    sourcemap: DEV,
+    tsconfig: 'src/node/tsconfig.json',
+    external: (id, parentId) =>
+      (parentId?.endsWith('.d.ts') &&
+        typesExternal.some((re) => re.test(id))) ||
+      undefined
   })
-)
 
-const dVueFiles = await glob('dist/client/**/*.d.vue.ts')
-await Promise.all(dVueFiles.map((file) => fs.rm(file)))
+  console.log('Node build complete.')
+}
 
-console.log('Client build complete.')
+async function buildClient() {
+  await mkdist({
+    addRelativeDeclarationExtensions: true,
+    cleanDist: !DEV,
+    declaration: true,
+    distDir: 'dist/client',
+    ext: 'js',
+    format: 'esm',
+    pattern: '**/*.{vue,ts,css,woff2}',
+    srcDir: 'src/client'
+  })
+
+  const dtsFiles = await glob('dist/client/**/*.d.ts')
+  await Promise.all(
+    dtsFiles.map(async (file) => {
+      const content = await fs.readFile(file, 'utf-8')
+      const updated = content.replace(/import\s+["'][^"']+["'];\n/g, '')
+      if (updated !== content) {
+        await fs.writeFile(file, updated)
+      }
+    })
+  )
+
+  const dVueFiles = await glob('dist/client/**/*.d.vue.ts')
+  await Promise.all(dVueFiles.map((file) => fs.rm(file)))
+
+  console.log('Client build complete.')
+}
+
+await Promise.all([buildNode(), buildClient()])
