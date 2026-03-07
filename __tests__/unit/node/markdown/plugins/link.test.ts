8d6a34d3a56ed6dc7a48a7cc4995e6a12c5b9e2e
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2026-03-08 01:36:13 +0530

    adjust tests

--- __tests__/unit/node/markdown/plugins/link.test.ts
+++ __tests__/unit/node/markdown/plugins/link.test.ts
@@ -1,13 +1,12 @@
+import { slugify } from '@mdit-vue/shared'
 import { MarkdownItAsync } from 'markdown-it-async'
 import { linkPlugin } from 'node/markdown/plugins/link'
 
 describe('node/markdown/plugins/link', () => {
-  const slugify = (str: string) => str.toLowerCase().replace(/\s+/g, '-')
+  const md = new MarkdownItAsync()
+  linkPlugin(md, {}, '/', slugify)
 
   test('preserves text-fragment hashes on markdown links', async () => {
-    const md = new MarkdownItAsync()
-    linkPlugin(md, {}, '/', slugify)
-
     const html = await md.renderAsync(
       '[58-61](/resources/server/user#:~:text=58*,time%20authentication%20token)',
       { cleanUrls: false }
@@ -20,11 +19,8 @@ describe('node/markdown/plugins/link', () => {
 
   // https://web.dev/articles/text-fragments#mixing_element_and_text_fragments
   test('preserves mixed element and text-fragment hashes', async () => {
-    const md = new MarkdownItAsync()
-    linkPlugin(md, {}, '/', slugify)
-
     const html = await md.renderAsync(
-      '[Section text](/guide/getting-started#Hello%20World:~:text=Hello%20World)',
+      '[Section](/guide/getting-started#Hello%20World:~:text=Hello%20World)',
       { cleanUrls: false }
     )
 
@@ -34,14 +30,9 @@ describe('node/markdown/plugins/link', () => {
   })
 
   test('continues to normalize regular heading hashes', async () => {
-    const md = new MarkdownItAsync()
-    linkPlugin(md, {}, '/', slugify)
-
     const html = await md.renderAsync(
       '[Section](/guide/getting-started#Hello%20World)',
-      {
-        cleanUrls: false
-      }
+      { cleanUrls: false }
     )
 
     expect(html).toContain('href="/guide/getting-started.html#hello-world"')
