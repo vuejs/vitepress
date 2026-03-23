bbdcb913e06cefd67a9b930b771228601a3fa563
chencu <82279230+chencu5958@users.noreply.github.com>
2026-03-23 20:36:40 +0800

    fix(format) format correction

--- src/node/markdown/plugins/containers.ts
+++ src/node/markdown/plugins/containers.ts
@@ -15,13 +15,7 @@ export const containerPlugin = (
     .use(...createContainer('danger', options?.dangerLabel || 'DANGER', md))
     .use(...createContainer('details', options?.detailsLabel || 'Details', md))
     .use(...createContainer('note', options?.noteLabel || 'NOTE', md))
-    .use(
-      ...createContainer(
-        'important',
-        options?.importantLabel || 'IMPORTANT',
-        md
-      )
-    )
+    .use(...createContainer('important', options?.importantLabel || 'IMPORTANT', md))
     .use(...createContainer('caution', options?.cautionLabel || 'CAUTION', md))
     // explicitly escape Vue syntax
     .use(container, 'v-pre', {
